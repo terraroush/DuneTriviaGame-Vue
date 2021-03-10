18fa5c8dc4d5526ff3eea91f87b62ea1e64d2fd2
Terra Roush <terra.roush@gmail.com>
2021-03-10 10:58:23 -0600

    added trivia.js file with trivia objects

---
+++ src/trivia.js
@@ -0,0 +1,144 @@
+export default [
+  {
+    question:
+      "Who played Paul Atreides in David Lynch's 1984 adaptation of Dune?",
+    answer: "Kyle MacLachlan",
+    answerShown: false,
+    difficulty: "easy",
+  },
+  {
+    question:
+      "Who plays Paul Atreides in the not yet released adaptaion of Dune?",
+    answer: "Timothée Chalamet",
+    answerShown: false,
+    difficulty: "medium",
+  },
+  {
+    question: "Who is your favorite actor in the 1984 movie Dune?",
+    answer: "Sting, but we'll also accept Sir Patrick Stewart",
+    answerShown: false,
+    difficulty: "easy",
+  },
+  {
+    question: "What movie did David Lynch pass on directing in favor of Dune?",
+    answer: "Return of the Jedi",
+    answerShown: false,
+    difficulty: "medium",
+  },
+  {
+    question: "Who is the director of the new Dune?",
+    answer: "Denis Villeneuve",
+    answerShown: false,
+    difficulty: "medium",
+  },
+  {
+    question: "What is the material being mined on Arrakis?",
+    answer: "The Spice Melange",
+    answerShown: false,
+    difficulty: "hard",
+  },
+  {
+    question: "What do the fremen do with their dead?",
+    answer: "Render them down for their water",
+    answerShown: false,
+    difficulty: "hard",
+  },
+  {
+    question:
+      "What is the exclusive religious and mystical sisterhood in Dune?",
+    answer: "The Bene Gesserit",
+    answerShown: false,
+    difficulty: "easy",
+  },
+  {
+    question: "What is a muad'dib?",
+    answer: "a type of desert mouse",
+    answerShown: false,
+    difficulty: "hard",
+  },
+  {
+    question: "What is melange made of?",
+    answer: "sandworms",
+    answerShown: false,
+    difficulty: "medium",
+  },
+  {
+    question: "What is a crysknife made of?",
+    answer: "a sandworm's tooth",
+    answerShown: false,
+    difficulty: "easy",
+  },
+  {
+    question: "What is Paul's tribal name among the Fremen?",
+    answer: "Usal",
+    answerShown: false,
+    difficulty: "medium",
+  },
+  {
+    question:
+      "What is the last thing Paul must do before he is accepted as a full Fremen?",
+    answer: "Ride a sandworm",
+    answerShown: false,
+    difficulty: "medium",
+  },
+  {
+    question: "What is the needle tipped with poison called",
+    answer: "Gom gabbar",
+    answerShown: false,
+    difficulty: "medium",
+  },
+  {
+    question: "What is Paul's son named?",
+    answer: "Leto",
+    answerShown: false,
+    difficulty: "hard",
+  },
+  {
+    question: "What does Leto become far into the future in the books of Dune?",
+    answer: "A sandworm",
+    answerShown: false,
+    difficulty: "hard",
+  },
+  {
+    question: "Who is the baron's grandson?",
+    answer: "Paul Atreides",
+    answerShown: false,
+    difficulty: "hard",
+  },
+  {
+    question: "Who is the emperor's truthsayer?",
+    answer: "The Reverend Mother Mohiam",
+    answerShown: false,
+    difficulty: "hard",
+  },
+  {
+    question: "Which Fremen leader does Paul form a close relationship?",
+    answer: "Stilgar",
+    answerShown: false,
+    difficulty: "hard",
+  },
+  {
+    question: "What color are Fremen eyes?",
+    answer: "all blue, including the sclera",
+    answerShown: false,
+    difficulty: "easy",
+  },
+  {
+    question: "Who is Chani's father?",
+    answer: "Dr. Kynes",
+    answerShown: false,
+    difficulty: "hard",
+  },
+  {
+    question: "Mentats are trained to be like what?",
+    answer: "computers",
+    answerShown: false,
+    difficulty: "medium",
+  },
+  {
+    question: "How can you spot a mentat?",
+    answer: "Their lips are stained red from sapho juice",
+    answerShown: false,
+    difficulty: "hard",
+  },
+].map((q, index) => ({ ...q, id: index + 1 }));
